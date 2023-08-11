4261bad65f3dc56b9757b926aeb0367a22acd5e5
Roman-Fedorenko-07 <rmn.fedorenko7@gmail.com>
2023-08-11 11:30:29 +0300

    fix: files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+        if (n < 0) {
+        let nN = n * (-1);
+        nN = nN + "";
+        return nN.split("").reverse().join("");
+        }
+        if (n > 0) {
+            n = n + "";
+            return n.split("").reverse().join("");
+        }
+    }
